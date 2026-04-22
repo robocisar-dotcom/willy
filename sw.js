bec3e3a1c2b743428459d70794cf949acb7f9ff4
robocisar-dotcom <robo.cisar@gmail.com>
2026-04-22 14:36:26 +0200

    Implement service worker caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const cacheName = 'willy-v1';
+const assets = ['./willi.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
